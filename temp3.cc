dd1d3a6d7872a7629a421dabaad5978557db1c30
pgj123 <dbfltkfkd01@gmail.com>
2023-02-16 00:28:41 +0900

    6

--- temp3.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-int main(void) {
-    int a[500000]  = {0,};
-    int b[10000]  = {0,};
-    cout << a[1000000-700000];
-}
